21cc3346450f6be48ae79a5f2b77691176f2d969
Igor <capatina7802@gmail.com>
2021-07-02 23:43:27 +0300

    Simple arrays: https://www.learn-php.org/en/Simple_arrays

---
+++ arrays.php
@@ -0,0 +1,11 @@
+<?php
+$even_numbers = [2,4,6,8,10];
+
+$male_names = ["Jake", "Eric", "Jhon"];
+$famale_names = ["Jessica", "Beth", "Sandra"];
+
+$names = array_merge($male_names, $famale_names);
+
+print_r($even_numbers);
+print_r($names);
+?>
